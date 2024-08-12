d4d396787e6fcb890ff6d310b21b19e72f2713a1
steve <steve.berenger@gmail.com>
2024-08-12 15:42:43 +0200

    translate

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -6,7 +6,7 @@ using var db = new BloggingContext();
 // Note: This sample requires the database to be created before running.
 Console.WriteLine($"Database path: {db.DbPath}.");
 
-// Create
+// Create translate = creation 
 Console.WriteLine("Inserting a new blog");
 db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
 db.SaveChanges();
--- EFGetStarted/obj/Debug/net8.0/EFGetStarted.AssemblyInfo.cs
+++ EFGetStarted/obj/Debug/net8.0/EFGetStarted.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cd111019e6ee1c0991db4b61dfb292205f06d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
